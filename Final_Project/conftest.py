3b866428a34ddb34f4536ee95ee5c23f091c0524
va.fomin <va.fomin@tensor.ru>
2025-12-25 08:11:28 +0300

    1

---
+++ Final_Project/conftest.py
@@ -0,0 +1,30 @@
+from webbrowser import Chrome
+
+import pytest
+from selenium import webdriver
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
+    parser.addoption(
+        "--language",
+        action="store",
+        default="en",
+        help="language to use for tests (e.g., en, ru)"
+    )
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
---
+++ Frameworks/test_login_stepik.py
@@ -0,0 +1,41 @@
+import time
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import math
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def answer():
+    return str(math.log(int(time.time())))
+Links =['https://stepik.org/lesson/236895/step/1', 'https://stepik.org/lesson/236896/step/1',
+                              'https://stepik.org/lesson/236897/step/1', 'https://stepik.org/lesson/236898/step/1',
+                              'https://stepik.org/lesson/236899/step/1', 'https://stepik.org/lesson/236903/step/1',
+                              'https://stepik.org/lesson/236904/step/1', 'https://stepik.org/lesson/236905/step/1']
+LOGIN_BUTTON = (By.CSS_SELECTOR, '.ember-view.navbar__auth.navbar__auth_login.st-link.st-link_style_button')
+
+
+def login(self, browser, link):
+    browser.get(f'{link}')
+    WebDriverWait(browser, 20).until(EC.element_to_be_clickable(LOGIN_BUTTON)).click()
+    login = browser.find_element(By.CSS_SELECTOR, 'input#id_login_email').send_keys("VycheslavFA@yandex.ru")
+    password = browser.find_element(By.CSS_SELECTOR, 'input#id_login_password').send_keys("Slavikfomin123!")
+    entry_button = browser.find_element(By.CSS_SELECTOR, 'button.sign-form__btn.button_with-loader ').click()
+
+class TestFindElement:
+    @pytest.mark.parametrize('link', Links)
+    def test_from_answer(self, browser, link):
+        login(self, browser, link)
+        WebDriverWait(browser, 10).until_not(EC.presence_of_all_elements_located(LOGIN_BUTTON))
+
+        WebDriverWait(browser, 20, 0.5).until(
+            EC.element_to_be_clickable((By.TAG_NAME, 'textarea'))
+        ).send_keys(answer())
+
+        send_button = WebDriverWait(browser, 20, 1.5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.submit-submission")))
+        send_button.click()
+
+        time.sleep(5)
+        correct = browser.find_element(By.CSS_SELECTOR,'p.smart-hints__hint')
+        correct_text = correct.text
+        assert correct_text == "Correct!"
